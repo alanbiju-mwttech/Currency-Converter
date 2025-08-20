e85335bd1dc4cfa9417094769d86f443746c1c3f
Alan Biju <alan.biju@mwttech.com>
2025-08-20 17:32:38 +0530

    Convert to Upper case from the Input field

--- sample.js
+++ sample.js
@@ -25,9 +25,9 @@ const convert = (from,to,amount) =>{
 
 const main = () => {
     submit.addEventListener("click", function(){
-        const from = document.getElementById("from").value;
-        const to = document.getElementById("to").value;
-        const amount = document.getElementById("amount").value
+        const from = document.getElementById("from").value.toUpperCase();
+        const to = document.getElementById("to").value.toUpperCase();
+        const amount = document.getElementById("amount").value.toUpperCase();
         convert(from, to, amount)
     })
 }
